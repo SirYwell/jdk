5c3f48563c4896e48cfe4431f6a748b8a595b0e7
SirYwell <hannesgreule@outlook.de>
2023-04-30 14:18:41 +0200

    Add benchmark

---
+++ test/micro/org/openjdk/bench/java/util/RandomGeneratorNextBytes.java
@@ -0,0 +1,50 @@
+package org.openjdk.bench.java.util;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class RandomGeneratorNextBytes {
+
+    @Param({"15", "1023", "1024"})
+    private static int size;
+
+    @Param({"random", "default"})
+    private static String generatorName;
+
+    private RandomGenerator generator;
+
+    private byte[] bytes;
+
+    @Setup
+    public void setup() {
+        this.bytes = new byte[size];
+        this.generator = switch (generatorName) {
+            case "random" -> new Random(0);
+            case "default" -> RandomGeneratorFactory.getDefault().create(0);
+            default -> throw new AssertionError("Unexpected value: " + generatorName);
+        };
+    }
+
+    @Benchmark
+    public byte[] nextBytes() {
+        generator.nextBytes(bytes);
+        return bytes;
+    }
+}
